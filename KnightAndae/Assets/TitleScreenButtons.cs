909a354008b04be150a02229f3926885345af8d1
IanFredericks <92319305+IanFredericks@users.noreply.github.com>
2021-11-05 12:53:02 -0500

    Added Title Screen
    Added the title screen and Town Scene to  the build. Clicking start will send the player into the Town Scene

---
+++ KnightAndae/Assets/TitleScreenButtons.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreenButtons : MonoBehaviour
+{
+    public string startLevel;
+    public string levelSelect;
+
+    public void NewGame()
+    {
+        Application.LoadLevel(startLevel);
+    }
+
+    //public void LevelSelect()
+    //{
+       // Application.LoadLevel(levelSelect);
+    //}
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
